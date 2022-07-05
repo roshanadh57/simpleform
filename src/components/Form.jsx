12923e3257ecf398db11002302d83510b46229ea
roshanadh57 <roshanadh57@gmail.com>
2022-07-05 16:24:53 +0545

    Added my project

---
+++ src/components/Form.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useState } from "react";
+import View from "./View";
+import { tableData } from "./View";
+
+function Form(props) {
+  const { setFormData, formData } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>1. Name of the candidate </label>
+        <input type="text" name="userName" onChange={handleChange} />
+        <br />
+        <label>2. Fathers Name</label>
+        <input type="text" name="fatherName" onChange={handleChange} />
+        <br />
+        <label>3. Mothers Name</label>
+        <input type="text" name="motherName" onChange={handleChange} />
+        <br />
+        <label>4. Guardians Name</label>
+        <input type="text" name="guardianName" onChange={handleChange} />
+        <br />
+        <label>5. Date of Birth</label>
+        <input type="date" name="dob" onChange={handleChange} />
+        <br />
+        <label>6. Address for correspondance</label>
+        <input type="text" name="temporaryAddress" onChange={handleChange} />
+        <br />
+        <label>7. Permanent Address</label>
+        <input type="text" name="permanentAddress" onChange={handleChange} />
+        <br />
+        <label>Mobile Number</label>
+        <input type="text" name="mobileNumber" onChange={handleChange} />
+        <br />
+        <label>8. Email Id</label>
+        <input type="email" name="email" onChange={handleChange} />
+        <br />
+        <label>9. Do you belong to economically weaker section</label>
+        <input
+          type="radio"
+          value="Yes"
+          name="weakFamily"
+          onChange={handleChange}
+        />
+        Yes
+        <input
+          type="radio"
+          value="No"
+          name="weakFamily"
+          onChange={handleChange}
+        />
+        No
+        <br />
+        <label>10. Anual Family Income Source</label>
+        <input type="text" name="income" onChange={handleChange} />
+        <br />
+        <label>11. Adhar Card</label>
+        <input type="text" name="adharCard" onChange={handleChange} />
+        <br />
+        <label>12. Blood Group</label>
+        <select name="bloodGroup" onChange={handleChange}>
+          <option value="A+ ve">A+ ve</option>
+          <option value="A- ve">A- ve</option>
+          <option value="B+ ve">B+ ve</option>
+          <option value="B- ve">B- ve</option>
+          <option value="o+ ve">o+ ve</option>
+          <option value="o- ve">o- ve</option>
+        </select>
+      </form>
+    </>
+  );
+}
+export default Form;
---
+++ src/components/Page.jsx
@@ -0,0 +1,67 @@
+import react, { Component } from "react";
+import Form from "./Form";
+import View from "./View";
+import { useState } from "react";
+
+function Page() {
+  const [formData, setFormData] = useState({
+    userName: " ",
+    fatherName: " ",
+    motherName: " ",
+    guardianName: " ",
+    dob: "",
+    temporaryAddress: " ",
+    permanentAddress: " ",
+    mobileNumber: " ",
+    email: " ",
+    weakFamily: " ",
+    income: " ",
+    adharCard: " ",
+    bloodGroup: " ",
+  });
+
+  const [tableData, setTableData] = useState(
+    {
+      semester: "",
+      marksObtained: "",
+      maximumMarks: "",
+      percentage: "",
+      year: "",
+      examinationRollNo: "",
+    },
+    {
+      semester: "",
+      marksObtained: "",
+      maximumMarks: "",
+      percentage: "",
+      year: "",
+      examinationRollNo: "",
+    },
+    {
+      semester: "",
+      marksObtained: "",
+      maximumMarks: "",
+      percentage: "",
+      year: "",
+      examinationRollNo: "",
+    }
+  );
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log({
+      ...formData,
+      ...tableData,
+    });
+  }
+
+  return (
+    <div>
+      <Form formData={formData} setFormData={setFormData} />
+      <View tableData={tableData} setTableData={setTableData} />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default Page;
